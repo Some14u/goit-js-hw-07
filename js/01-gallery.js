95652043c90221461749e3bff125b07ba4c612ca
Some14u <some1@email.ua>
2022-05-18 15:16:05 +0300

    Закончены оба задания

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,55 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+let modal;
+const gallery = document.querySelector("div.gallery");
+const galleryMockup = buildGalleryMockup(galleryItems);
+
+console.log(VK_CONTROL);
+
+gallery.innerHTML = galleryMockup.join("\n");
+
+gallery.addEventListener("click", showBigImage);
+
+function showBigImage(event) {
+  event.preventDefault();
+  modal = basicLightbox.create(
+    `<img src="${event.target.dataset.source}">`,
+    {
+      onShow: addEscCallback,
+      onClose: removeEscCallback,
+    }
+  );
+  modal.show();
+}
+
+function addEscCallback() {
+  window.addEventListener("keydown", keydownEscHandler)
+};
+
+function removeEscCallback() {
+  window.removeEventListener("keydown", keydownEscHandler)
+};
+
+function keydownEscHandler({key}) {
+  if (key === "Escape") modal.close();
+};
+
+function buildGalleryMockup(items) {
+  return items.map(makeItemMockup);
+};
+
+function makeItemMockup ({preview, original, description}) {
+  return `
+    <div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>`
+};
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const CAPTION_DELAY = 250;
+
+let modal;
+const gallery = document.querySelector("ul.gallery");
+const galleryMockup = buildGalleryMockup(galleryItems);
+
+gallery.innerHTML = galleryMockup.join("\n");
+
+new SimpleLightbox(
+  'ul.gallery a',
+  {
+    captions: true,
+    captionsData: "alt",
+    captionDelay: CAPTION_DELAY,
+    disableScroll: false, // Пришлось отключить, потому что у них оно коряво реализовано, и сдвигает всю галлерею
+  }
+);
+
+function buildGalleryMockup(items) {
+  return items.map(makeItemMockup);
+};
+
+function makeItemMockup ({preview, original, description}) {
+  // Вынужден был добавить инлайн-стили, иначе вылезают косяки с разметкой
+  return `
+  <li>
+    <a class="gallery__item" href="${original}" style="display: block; height: 100%">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+      />
+    </a>
+  </li>`
+};
+
